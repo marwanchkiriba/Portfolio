8620e8470c10909ecc232fb32976afab16ab1cee
Abhoy Sarkar <sarkar.ab07@gmail.com>
2025-11-20 12:05:02 +0530

    sitemap

--- app/_document.tsx
+++ app/_document.tsx
@@ -6,7 +6,6 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://www.googletagmanager.com" />
-        {/* Preload critical CSS */}
         <link rel="preload" href="/styles/globals.css" as="style" />
       </Head>
       <body>
